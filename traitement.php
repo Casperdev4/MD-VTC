74c5e0bfdc14401569020ba67a934590c6f20339
Casperdev4 <allam.bilal91@gmail.com>
2025-06-19 10:54:46 +0200

    m

--- traitement.php
+++ traitement.php
@@ -94,7 +94,7 @@ try {
 
     $mail->setFrom('contact@webprime.fr', 'MD-VTC');
     $mail->addAddress('contact.mdvtc.privatedriver@gmail.com');
-    $mail->addAddress('webprime91@hotmail.com');
+    $mail->addAddress('lennygui91@hotmail.com');
 
     $mail->CharSet = 'UTF-8';
     $mail->isHTML(true);
